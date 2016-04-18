9b9066155fab98144472328e1631be982f0326a8
Hyunkyu Yoon <hkyoon85@gmail.com>
2016-04-18 17:25:06 +0900

    Implement 404 and 500 with express

--- meadowlark.js
+++ meadowlark.js
@@ -25,17 +25,15 @@ app.get('/about', function (req, res) {
 
 // custom 404 page
 app.use(function (req, res) {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 // custom 500 page
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function () {
